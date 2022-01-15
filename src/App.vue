081f4c0bed5363b32b176759409770d5265c0dce
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-01-15 10:22:34 +0700

    Online Status

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,3 @@
 // Import Main styles for this application
 @import 'styles/style';
 </style>
-
-<script>
-  export default {
-    methods: {
-      logout() {
-        console.log('logout')
-      }
-    },
-    mounted() {
-      window.addEventListener('beforeunload', this.logout)
-    }
-  }
-</script>
