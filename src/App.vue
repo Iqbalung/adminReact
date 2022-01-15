7266049c5033471e7bafb851786ebc4a4eabf493
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-01-15 10:12:31 +0700

    Online

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,16 @@
 // Import Main styles for this application
 @import 'styles/style';
 </style>
+
+<script>
+  export default {
+    methods: {
+      logout() {
+        console.log('logout')
+      }
+    },
+    mounted() {
+      window.addEventListener('beforeunload', this.logout)
+    }
+  }
+</script>
